c50aacce617cb2d8443e4da40426cd3ef61ce7b0
GabrielAceti <gabriel.aceti@hotmail.com>
2021-08-23 15:44:20 -0300

    refactoring userController to work with callbacks

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -5,57 +5,69 @@ const bcrypt = require('bcrypt');
 class UserController {
 
     async post(req, res) {
-       
-        try {           
-            const createdUser = await users.create(req.body);
-            return res.status(200).json(createdUser);
-        }
-        catch (err) {
-            return res.status(400).json(err);
-        }
 
+        await users.create(req.body, (err, data) => {
+            if (err) {
+                return res.status(400).json(err);
+            }
+            else if (!data) {
+                return res.status(200).json({ error: "O usuário não foi criado" });
+            }
+            else {
+                return res.status(200).json(data);
+            }
+        });
     }
 
     async get(req, res) {
-        try {
-            const user = await users.find();            
-            return res.status(200).json(user);
-        }
-        catch (err) {
-            return res.status(400).json(err);
-        }
-    }
 
-    async findOne(req, res){
-        const { _id } = req.query;
-
-        try{
-            const user = await users.findOne({ _id });
-            return res.status(200).json(user);
-        }
-        catch(err){
-            return res.status(400).json(err);
-        }
-    }
-
-    async auth(req, res){
-        const {email, passWord} = req.body;
-
-        if(!email || !passWord) return res.send({error: 'Dados insuficientes!'});
-
-        users.findOne({email}, (err, data) => {
-            if(err){
+        await users.find({}, (err, data) => {
+            if (err) {
                 return res.status(400).json(err);
             }
-            else if(!data){
-                return res.status(200).json({error: "Usuário não registrado!"});
+            else if (!data) {
+                return res.status(200).json({ error: "Usuário(s) não encontrado(s)!" });
             }
-            else{
+            else {
+                return res.status(200).json(data);
+            }
+        });
+    }
+
+    async findOne(req, res) {
+        const { _id } = req.query;
+
+        await users.findOne({ _id }, (err, data) => {
+            if (err) {
+                return res.status(400).json(err);
+            }
+            else if (!data) {
+                return res.status(200).json({ error: "Usuário não encontrado!" });
+            }
+            else {
+                return res.status(200).json(data);
+            }
+        });
+    }
+
+    async auth(req, res) {
+        const { email, passWord } = req.body;
+
+        if (!email || !passWord) return res.send({ error: 'Dados insuficientes!' });
+
+        users.findOne({ email }, (err, data) => {
+            if (err) {
+                return res.status(400).json(err);
+            }
+            else if (!data) {
+                return res.status(200).json({ error: "Usuário não registrado!" });
+            }
+            else {
                 bcrypt.compare(passWord, data.passWord, (err, same) => {
-                    if(!same){
-                        return res.status(400).json({error: "Senha incorreta!"});
+                    if (!same) {
+                        return res.status(400).json({ error: "Senha incorreta!" });
                     }
-                    else{
+                    else {
                         data.passWord = undefined;
                         return res.status(200).json(data);
                     }
