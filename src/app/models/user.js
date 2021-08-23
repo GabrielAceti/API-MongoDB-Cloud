0ba2842ba05abd2572ec3d414cd0e3808a4a62aa
GabrielAceti <gabriel.aceti@hotmail.com>
2021-08-23 15:20:10 -0300

    creating password encryption

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,9 +1,26 @@
 const mongoose = require('../../data/index');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
 email: {type: String, require: true, unique: true, lowercase: true},
 passWord: {type: String, require: true, select: false },
 createdAt: {type: Date, default: Date.now }
+});
+
+userSchema.pre('save', function (next) {
+    let user = this;
+    if(!user.isModified('passWord')) return next();
+
+    bcrypt.hash(user.passWord, 10, (err, encrypted) => {
+        if(!err){
+            user.passWord = encrypted;
+            return next();
+        }
+        else{
+            console.log(err);
+        }
+        
+    });
 });
 
 module.exports = mongoose.model('User', userSchema);
